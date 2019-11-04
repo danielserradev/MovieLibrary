3ed840556f0d0211a2bada030588e564828b0714
danny serra <danielserradev@gmail.com>
2019-11-04 16:02:04 -0600

    did some edititng to the html and js file

--- Client/app.js
+++ Client/app.js
@@ -14,14 +14,44 @@
             data: JSON.stringify(dict),
             success: function( data, textStatus, jQxhr ){
                 $('#response pre').html( data );
+                console.log("Success");
             },
             error: function( jqXhr, textStatus, errorThrown ){
                 console.log( errorThrown );
             }
         });
 
+
         e.preventDefault();
     }
+    $('#create-form').submit( processForm );
+})(jQuery);
 
-    $('#my-form').submit( processForm );
+(function($){
+    function processForm( e ){
+        var dict = {
+        	Title : this["title"].value,
+        	Director: this["director"].value,
+          Genre: this["genre"].value
+        };
+
+        $.ajax({
+            url: 'https://localhost:44352/api/movie',
+            dataType: 'json',
+            type: 'get',
+            contentType: 'application/json',
+            data: JSON.stringify(dict),
+            success: function( data, textStatus, jQxhr ){
+                $('#response pre').html( data );
+
+            },
+            error: function( jqXhr, textStatus, errorThrown ){
+                console.log( errorThrown );
+            }
+        });
+
+
+        e.preventDefault();
+    }
+    $('#get-form').submit( processForm );
 })(jQuery);
