505e520ae55184cf583fce3dcfd42006974a5db4
danny serra <danielserradev@gmail.com>
2019-11-04 10:21:07 -0600

    initial commit

---
+++ WebAPISample/Models/Movie.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPISample.Models
+{
+    public class Movie
+    {
+        //hello
+        public int MovieId { get; set; }
+        public string Title { get; set; }
+        public string Director { get; set; }
+    }
+}
